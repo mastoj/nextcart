ed10ec6ce8f0ce352d3927461779d766a83cb3ff
Tomas Jansson <tomas.jansson@gmail.com>
2022-12-06 11:21:19 +0100

    Remove some more endpoints for cart

--- src/NextCart.Api/Cart/CartApi.cs
+++ src/NextCart.Api/Cart/CartApi.cs
@@ -6,23 +6,53 @@ using Microsoft.AspNetCore.Routing;
 
 namespace Cart;
 
+public record Cart();
 public static class CartApi
 {
     public static RouteGroupBuilder MapCart(this IEndpointRouteBuilder routes)
     {
         var group = routes.MapGroup("/cart");
 
+        group.MapPost("/", CreateCart);
         group.MapGet("/{id}", GetCart);
+        group.MapPost("/{id}/clear", ClearCart);
+
+        group.MapPost("/{id}/items", AddItem);
+        group.MapPut("/{id}/items/{itemId}", UpdateItem);
+        group.MapDelete("/{id}/items/{itemId}", RemoveItem);
 
         return group;
     }
 
-    public static Results<Ok<string>, NotFound> GetCart(string id)
+    private static Results<Ok<Cart>, NotFound> CreateCart(string id)
     {
-        if (id == "1")
-        {
-            return TypedResults.Ok("Cart 1");
-        }
-        else return TypedResults.NotFound();
+        var cart = new Cart();
+        return TypedResults.Ok(cart);
+    }
+
+    private static Results<Ok<Cart>, NotFound> GetCart(string id)
+    {
+        var cart = new Cart();
+        return TypedResults.Ok(cart);
+    }
+
+    private static async Task ClearCart(HttpContext context)
+    {
+        await context.Response.WriteAsJsonAsync(new Cart());
+    }
+
+    private static Results<Ok<string>, NotFound> AddItem()
+    {
+        return TypedResults.Ok("Hello");
+    }
+
+    private static Results<Ok<string>, NotFound> UpdateItem()
+    {
+        return TypedResults.Ok("Hello");
+    }
+
+    private static Results<Ok<string>, NotFound> RemoveItem()
+    {
+        return TypedResults.Ok("Hello");
     }
 }
